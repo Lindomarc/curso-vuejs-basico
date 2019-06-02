39154ebfb869670b813626328cba2451a81835e2
lindomar corsino <lindomarc@outlook.com>
2019-06-02 15:26:21 -0300

    auta-11

--- index.php
+++ index.php
@@ -10,21 +10,42 @@
 <body>
 
     <div id="app" class="container">
-    <div v-show="created">
-            Criado com sucesso "v-show"
-        </div>        
-        <div v-if="created">
-            Criado com sucesso "v-if"
-        </div>
+    https://br.vuejs.org/v2/guide/instance.html
     </div>
 
     <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.js'></script>
+
     <script>
         const app = new Vue({
+            beforebeforeUpdateCreate(){
+                console.log('beforeCreate');
+            },
+            created(){
+                console.log('created');
+            },
+            
             el:'#app',
-            data:{
-                created: false
+            
+            beforeMount(){
+                console.log('beforeMount');
+            },
+            mounted(){
+                console.log('mounted');
+            },
+            beforeUpdate(){
+                console.log('beforeUpdate');
+            },
+            updated(){
+                console.log('updated');
+            },
+            beforeDestroy(){
+                console.log('beforeDestroy');
+            },
+            destroyed(){
+                console.log('destroyed');
             }
+
+
         });
     </script>
 </body>
